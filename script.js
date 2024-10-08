d9f4a42f0ef4c897c472fc60a2ff3a48dc572ec6
Zayn <ltlehro@gmail.com>
2024-10-08 19:52:59 +0500

    Completed lesson on arrow functions

--- script.js
+++ script.js
@@ -579,3 +579,55 @@ if (age < 18) {
 
 welcome(); // ok now
 
+// Arrow Functions
+
+let sum = (a, b) => a + b;
+
+/* This arrow function is a shorter form of:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+alert( sum(1, 2) ); // 3
+
+// Empty Paranthesis
+
+let sayHi = () => alert("Hello!");
+
+sayHi();
+
+// Dynamic Function
+
+let age = prompt("What is your age?", 18);
+
+let welcome = (age < 18) ?
+  () => alert('Hello!') :
+  () => alert("Greetings!");
+
+welcome();
+
+// Multi-line arrow function
+
+let sum = (a, b) => {  // the curly brace opens a multiline function
+  let result = a + b;
+  return result; // if we use curly braces, then we need an explicit "return"
+};
+
+alert( sum(1, 2) ); // 3
+
+// Arrow function (for cleaner code)
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
+
+
