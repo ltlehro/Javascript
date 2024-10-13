7c8093d63ad5e0815369105132e133d29d3892f9
Zayn <ltlehro@gmail.com>
2024-10-14 04:49:55 +0500

    Completed lessons on string algorithms

--- datatypes.js
+++ datatypes.js
@@ -79,3 +79,151 @@ alert( parseFloat('12.5em') ); // 12.5
 alert( parseInt('12.3') ); // 12, only the integer part is returned
 alert( parseFloat('12.3.4') ); // 12.3, the second point stops the reading
 
+// advantage of using backticks is that they allow a string to span multiple lines
+
+let guestList = `Guests:
+ * John
+ * Pete
+ * Mary
+`;
+
+alert(guestList); // a list of guests, multiple lines
+
+//these two lines are equal, just written differently
+
+let str1 = "Hello\nWorld"; // two lines using a "newline symbol"
+
+// two lines using a normal newline and backticks
+let str2 = `Hello
+World`;
+
+alert(str1 == str2); // true
+
+// Accessing characters
+
+let str = `Hello`;
+
+// the first character
+alert( str[0] ); // H
+alert( str.at(0) ); // H
+
+// the last character
+alert( str[str.length - 1] ); // o
+alert( str.at(-1) );
+
+// iterate over characters using for..of
+
+for (let char of "Hello") {
+    alert(char); // H,e,l,l,o (char becomes "H", then "e", then "l" etc)
+  }
+
+  // Strings can’t be changed in JavaScript. It is impossible to change a character
+let str = 'Hi';
+
+str[0] = 'h'; // error
+alert( str[0] ); // doesn't work
+
+// Methods toLowerCase() and toUpperCase() change the case
+
+alert( 'Interface'.toUpperCase() ); // INTERFACE
+alert( 'Interface'.toLowerCase() ); // interface
+alert( 'Interface'[0].toLowerCase() ); // 'i'
+
+// str.indexOf
+
+let str = 'Widget with id';
+
+alert( str.indexOf('Widget') ); // 0, because 'Widget' is found at the beginning
+alert( str.indexOf('widget') ); // -1, not found, the search is case-sensitive
+alert( str.indexOf("id") ); // 1, "id" is found at the position 1 (..idget with id)
+
+// find element at a position using indexOf
+
+let str = 'As sly as a fox, as strong as an ox';
+
+let target = 'as'; // let's look for it
+
+let pos = 0;
+while (true) {
+  let foundPos = str.indexOf(target, pos);
+  if (foundPos == -1) break;
+
+  alert( `Found at ${foundPos}` );
+  pos = foundPos + 1; // continue the search from the next position
+}
+
+// more simpler algorithm
+
+let str = "As sly as a fox, as strong as an ox";
+let target = "as";
+
+let pos = -1;
+while ((pos = str.indexOf(target, pos + 1)) != -1) {
+  alert( pos );
+}
+
+// with if test
+
+let str = "Widget with id";
+
+if (str.indexOf("Widget")) {
+    alert("We found it"); // doesn't work!
+}
+
+// it returns index 0, but if considers 0 to be false
+
+// we should actually check for -1, like this
+
+if (str.indexOf("Widget") != -1) // works now
+
+// includes, startsWith, endsWith
+
+alert( "Widget with id".includes("Widget") ); // true
+
+alert( "Hello".includes("Bye") ); // false
+
+alert( "Widget".includes("id") ); // true
+
+alert( "Widget".includes("id", 3) ); // false, from position 3 there is no "id"
+
+alert( "Widget".startsWith("Wid") ); // true, "Widget" starts with "Wid"
+
+alert( "Widget".endsWith("get") ); // true, "Widget" ends with "get"
+
+// getting a substring
+
+let str = "stringify";
+alert( str.slice(0, 5) ); // 'strin', the substring from 0 to 5 (not including 5)
+alert( str.slice(0, 1) ); // 's', from 0 to 1, but not including 1, so only character at 0
+
+let str = "stringify";
+alert( str.slice(2) ); // 'ringify', from the 2nd position till the end
+
+let str = "stringify";
+
+// start at the 4th position from the right, end at the 1st from the right
+
+alert( str.slice(-4, -1) ); // 'gif'
+
+let str = "stringify";
+
+// these are same for substring
+alert( str.substring(2, 6) ); // "ring"
+alert( str.substring(6, 2) ); // "ring"
+
+// ...but not for slice:
+alert( str.slice(2, 6) ); // "ring" (the same)
+alert( str.slice(6, 2) ); // "" (an empty string)
+
+//  strings algorithm
+
+let str = '';
+
+for (let i = 65; i <= 220; i++) {
+  str += String.fromCodePoint(i);
+}
+alert( str );
+
+// Output:
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
+// ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ
