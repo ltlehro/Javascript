cfd427403dbccd2ba0444043da248ef0778500a4
Zayn <ltlehro@gmail.com>
2024-11-06 19:54:54 +0500

    added function topSalary to find the name with the maximum salary in an object

--- objects.js
+++ objects.js
@@ -991,4 +991,27 @@ alert(title);  // Menu
 alert(w);      // 100
 alert(h);      // 200
 
+// The maximal salary
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+function topSalary(salaries) {
+
+  let maxSalary = 0;
+  let maxName = null;
+
+  for(const [name, salary] of Object.entries(salaries)) {
+    if (maxSalary < salary) {
+      maxSalary = salary;
+      maxName = name;
+    }
+  }
+
+  return maxName;
+}
+
 
